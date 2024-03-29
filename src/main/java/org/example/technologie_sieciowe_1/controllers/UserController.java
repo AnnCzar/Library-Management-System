ee8e3dfdf7e0217c4991d978638f772e7d01c708
AnnCzar <268319@student.pwr.edu.pl>
2024-03-29 23:30:07 +0100

    final version

--- src/main/java/org/example/technologie_sieciowe_1/controllers/UserController.java
+++ src/main/java/org/example/technologie_sieciowe_1/controllers/UserController.java
@@ -3,18 +3,17 @@ package org.example.technologie_sieciowe_1.controllers;
 import org.example.technologie_sieciowe_1.controllers.dto.create.CreateUserDto;
 import org.example.technologie_sieciowe_1.controllers.dto.get.GetUserDto;
 import org.example.technologie_sieciowe_1.controllers.dto.respone.CreateUserResponseDto;
-//import org.example.technologie_sieciowe_1.security.PasswordConfig;
-//import org.example.technologie_sieciowe_1.security.PasswordConfig;
+
 import org.example.technologie_sieciowe_1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.annotation.Secured;
-//import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Secured("ROLE_LIBRARIAN")
 public class UserController {
     private final UserService userService;
 
@@ -24,28 +23,23 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping("/getAll")
-//    @Secured("LIBRARIAN")
     public @ResponseBody Iterable<GetUserDto> getAll(){
         return userService.getAll();
     }
 
     @GetMapping("/getById")
-//    @Secured("LIBRARIAN")
     public @ResponseBody GetUserDto getById(Integer id){
         return userService.getById(id);
     }
 
     @PostMapping("/add")
-//    @Secured("LIBRARIAN")
     @ResponseStatus(code = HttpStatus.CREATED)
     public @ResponseBody CreateUserResponseDto add(@RequestBody CreateUserDto userEntity){
 
         return userService.add(userEntity);
     }
 
-
     @DeleteMapping("/delete")
-//    @Secured("LIBRARIAN")
     public ResponseEntity<Void> delete (Integer id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
--- src/main/java/org/example/technologie_sieciowe_1/service/UserService.java
+++ src/main/java/org/example/technologie_sieciowe_1/service/UserService.java
@@ -1,19 +1,12 @@
 package org.example.technologie_sieciowe_1.service;
 
-import org.apache.catalina.User;
+
 import org.example.technologie_sieciowe_1.controllers.dto.create.CreateUserDto;
 import org.example.technologie_sieciowe_1.controllers.dto.get.GetUserDto;
 import org.example.technologie_sieciowe_1.controllers.dto.respone.CreateUserResponseDto;
-import org.example.technologie_sieciowe_1.infrastructure.entity.ReviewEntity;
 import org.example.technologie_sieciowe_1.infrastructure.entity.UserEntity;
-import org.example.technologie_sieciowe_1.infrastructure.repositories.ReviewRepository;
 import org.example.technologie_sieciowe_1.infrastructure.repositories.UserRepository;
-//import org.example.technologie_sieciowe_1.security.PasswordConfig;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
@@ -24,16 +17,9 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-//    private final PasswordEncoder passwordEncoder;
-
-    @Value("${jwt.token.key}")
-    private String key;
-
-
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -42,9 +28,6 @@ public class UserService {
         var users = userRepository.findAll();
         return StreamSupport.stream(users.spliterator(), false)
                 .map(user -> new GetUserDto(user.getId(),
-//                        user.getUserName(),
-//                        user.getPassword(),
-//                        user.getRole(),
                         user.getEmail(),
                         user.getFullUserName(),
                         user.getRental(),
@@ -55,9 +38,6 @@ public class UserService {
     public GetUserDto getById(Integer id){
         var userEntity = userRepository.findById(id).orElse(null);
         return new GetUserDto(userEntity.getId(),
-//                userEntity.getUserName(),
-//                userEntity.getPassword(),
-//                userEntity.getRole(),
                 userEntity.getEmail(),
                 userEntity.getFullUserName(),
                 userEntity.getRental(),
@@ -70,18 +50,12 @@ public class UserService {
 
         var userEntity = new UserEntity();
         userEntity.setEmail(user.getEmail());
-//        userEntity.setUserName(user.getUserName());
         userEntity.setFullUserName(user.getFullUserName());
-//        userEntity.setPassword(passwordEncoder.encode(user.getPassword())); // Kodowanie hasła
-//        userEntity.setRole(user.getRole());
         userEntity.setRental(user.getRental());
         userEntity.setReview(user.getReview());
 
         var newUser = userRepository.save(userEntity);
         return new CreateUserResponseDto(newUser.getId(),
-//                newUser.getUserName(),
-//                newUser.getPassword(),
-//                newUser.getRole(),
                 newUser.getEmail(),
                 newUser.getFullUserName(),
                 newUser.getRental(),
